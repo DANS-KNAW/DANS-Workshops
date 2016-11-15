7022e6d84f04fcf5d7060369615bbb4cec0474c0
Joke Pol <joke.pol@dans.knaw.nl>
2016-11-15 15:29:45 +0100

    rxJava to rxScala

--- src/main/scala/workshop4/assignments/solutions/RxEcosystem/RxFileUtils.scala
+++ src/main/scala/workshop4/assignments/solutions/RxEcosystem/RxFileUtils.scala
@@ -1,68 +1,51 @@
 package workshop4.assignments.solutions.RxEcosystem
 
 import java.io.File
-import java.nio.file.Path
-import java.util
 
-import rx.Observer
-import rx.fileutils.{FileSystemEvent, FileSystemEventKind, FileSystemWatcher}
+import rx.fileutils.{FileSystemEventKind, FileSystemWatcher}
+import rx.lang.scala.JavaConverters._
 import rx.schedulers.Schedulers
-import scala.collection.JavaConverters._
 
+import scala.collection.JavaConverters._
 import scala.language.implicitConversions
 
-class FileSystemEventObserver extends Observer[FileSystemEvent] {
-
-
-  override def onCompleted(): Unit =
-    println("completed")
-
-  override def onError(throwable: Throwable): Unit =
-    println(s"got exception $throwable ${throwable.getMessage}")
-
-  /**
-    * If a file exists on startup and gets changed after startup,
-    * you get a create for the folder and for the file both with an absolute path
-    * then a delete of the file with a relative path though it still exists.
-    *
-    * Another change results in a modify for the folder and file with absolute paths.
-    *
-    * Deleting a file causes a modify of the folder and delete for the file.
-    *
-    * Copy pasting a file a create (3,28 GB) at folder and file level and just a modify at file level,
-    * looks like all at once.
-    */
-  override def onNext(event: FileSystemEvent): Unit =
-  println(s"Got event ${event.getFileSystemEventKind} for ${event.getPath}")
-}
-
 /**
-  * partial copy-paste-translate of the java testcase,
-  * no more documentation found, not even JavaDoc
+  * If a file exists on startup and gets changed after startup,
+  * you get a create for the folder and for the file both with an absolute path
+  * then a delete of the file with a relative path though it still exists.
+  *
+  * Another change results in a modify for the folder and file with absolute paths.
+  *
+  * Deleting a file causes a modify of the folder and delete for the file.
+  *
+  * Copy pasting a file a create (3,28 GB) at folder and file level and just a modify at file level,
+  * looks like all at once.
   */
-object FileSystemEventObserver {
+object Example extends App {
 
-  def main(args: Array[String]): Unit = {
+  // val dir = new File("target/test/FileUtils/*") creates folder with name '*' !!!
+  val dir = new File("target/test/FileUtils")
+  dir.mkdirs()
 
+  val subscription = FileSystemWatcher
+    .newBuilder()
+    .addPaths(Map((dir.toPath, FileSystemEventKind.values()), (dir.toPath, FileSystemEventKind.values())).asJava)
+    .addPath(dir.toPath, FileSystemEventKind.values(): _*) // triple time the same keyvalue pairs, receiving events once
+    //.addPath(dir.toPath, FileSystemEventKind.ENTRY_DELETE) // would overwrite the previous
+    .withScheduler(Schedulers.io())
+    .withCurrentFsScanning(false) // did not see different behaviour with either value
+    .build()
+    .asScala
+    .doOnNext { e => println(s"Got event ${e.getFileSystemEventKind} for ${e.getPath}") }
+    .doOnError { t => println(s"got exception $t with message ${t.getMessage}") }
+    .doOnCompleted(println(s"got completed"))
+    .subscribe()
 
-    val dir = new File("target/test/FileUtils")
-    dir.mkdirs()
+  // run until receiving input (demonize?), causes InterruptedException (when executed with IDE)
+  System.in.read()
 
-    val subscription = FileSystemWatcher
-      .newBuilder()
-      .addPaths(Map((dir.toPath, FileSystemEventKind.values()), (dir.toPath, FileSystemEventKind.values())).asJava)
-      .addPath(dir.toPath, FileSystemEventKind.values(): _*) // triple time the same key, uses the last
-      //.addPath(dir.toPath, FileSystemEventKind.ENTRY_DELETE) // would overwrite the previous
-      .withScheduler(Schedulers.io())
-      .withCurrentFsScanning(false) // did not see different behaviour with either value
-      .build().subscribe(new FileSystemEventObserver)
+  // comes before processing the interrupt mentioned above
+  println("Received input, shutting down")
 
-    // run until receiving input (demonize?), causes InterruptedException (when executed with IDE)
-    System.in.read()
-
-    // comes before processing the interrupt
-    println("Received input, shutting down")
-
-    subscription.unsubscribe()
-  }
+  subscription.unsubscribe()
 }
